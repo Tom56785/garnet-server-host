e7310f84691fd97e4acff21cd775633287e29b71
Tom Albans <albans.thomas@gmail.com>
2024-05-27 22:36:53 +0100

    Added Password to available configuration options

---
+++ GarnetServerHost/Options/ExtendedGarnetServerOptions.cs
@@ -0,0 +1,16 @@
+using Garnet.server;
+using Microsoft.Extensions.Logging;
+
+namespace GarnetServerHost.Options;
+
+public class ExtendedGarnetServerOptions : GarnetServerOptions
+{
+	public const string ConfigBinding = "Garnet";
+	public string Password { get; set; }
+
+	public ExtendedGarnetServerOptions(ILogger logger = null)
+        : base(logger)
+    {
+        base.logger = logger;
+    }
+}
